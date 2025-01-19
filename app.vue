3873e8fe7388470176a61a9dbc42074ebfcab4fc
Maria Rios Navarro <propscode254@gmail.com>
2025-01-19 17:36:35 +0100

    add headline

--- app.vue
+++ app.vue
@@ -1,4 +1,9 @@
 <template>
+  <h1
+    class="flex items-center justify-center bg-sky-500 text-white text-2xl h-16"
+  >
+    Nuxt Calculator App
+  </h1>
   <div class="min-h-screen bg-gray-700 flex items-center justify-center">
     <Calculator />
   </div>
